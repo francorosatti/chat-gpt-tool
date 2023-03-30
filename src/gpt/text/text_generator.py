78c3f44dfad6eae5856f7f3fdd01a43414b76ba8
franrosattiLS <franco.rosatti@livestories.com>
2023-03-30 02:01:32 -0300

    Add chat generator

---
+++ src/gpt/text/text_generator.py
@@ -0,0 +1,23 @@
+class TextGenerator:
+    # Constants
+    URL_CREATE_COMPLETION = "/chat/completions"
+
+    MODEL_GPT_3_5_TURBO = "gpt-3.5-turbo"
+
+    def __init__(self, http_client):
+        self.client = http_client
+
+    def create_completion(self, prompt: str):
+        body = {
+            "model": self.MODEL_GPT_3_5_TURBO,
+            "messages": [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Who won the world series in 2020?"},
+                {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+                {"role": "user", "content": prompt}],
+            "n": 1,
+            "max_tokens": 50,
+            "frequency_penalty": 1.0,
+            "user": "test_user",
+        }
+        return self.client.post(self.URL_CREATE_COMPLETION, body)
--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ import webbrowser
 
 from gpt.image.image_generator import ImageGenerator
 from clients.http_client import HttpClient
+from src.gpt.text.text_generator import TextGenerator
+
+client = HttpClient(credentials.OPEN_AI_URL, credentials.OPEN_AI_API_KEY)
 
 
-def run():
-    client = HttpClient(credentials.OPEN_AI_URL, credentials.OPEN_AI_API_KEY)
-
+def run_image():
     generator = ImageGenerator(client)
 
     prompt = input("Enter your prompt: ")
@@ -22,5 +23,18 @@ def run():
         print(f"couldn't generate image: {e}")
 
 
+def run_text():
+    generator = TextGenerator(client)
+
+    prompt = input("Enter your prompt: ")
+    print("Processing your prompt...")
+
+    try:
+        response = generator.create_completion(prompt)
+        print(f"Chat GPT says: {response['choices'][0]['message']['content']}")
+    except Exception as e:
+        print(f"couldn't create completion: {e}")
+
+
 if __name__ == "__main__":
-    run()
+    run_text()
